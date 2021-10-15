9fdb177ef237c89c88f397cf9f99e7d285b6d870
akram09 <a.boutouchent@esi-sba.dz>
2021-10-15 16:32:46 +0100

    Disabling proptypes from eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,22 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-  },
-  extends: [
-    "plugin:react/recommended",
-    "airbnb",
-    "plugin:prettier/recommended",
-  ],
-  parserOptions: {
-    ecmaFeatures: {
-      jsx: true,
+    env: {
+        browser: true,
+        es2021: true,
     },
-    ecmaVersion: 2021,
-    sourceType: "module",
-  },
-  plugins: ["react"],
-  rules: {},
+    extends: [
+        'plugin:react/recommended',
+        'airbnb',
+        'plugin:prettier/recommended',
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 2021,
+        sourceType: 'module',
+    },
+    plugins: ['react'],
+    rules: {
+        'react/prop-types': 0,
+    },
 }
