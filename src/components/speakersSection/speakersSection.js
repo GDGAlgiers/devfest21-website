b3e64f144f1d5e7f241d5673c306dfb81a4a5ff1
islam36 <jm_boumendjel@esi.dz>
2021-10-20 13:17:36 +0100

    finish speakers section

--- src/components/speakersSection/speakersSection.js
+++ src/components/speakersSection/speakersSection.js
@@ -4,7 +4,7 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import { Navigation } from 'swiper';
 import 'swiper/css/bundle';
 import Speaker from './speaker';
-import './swiper.css';
+import './style.css';
 
 const ContentDiv = styled.div`
     width: 250px;
@@ -20,65 +20,66 @@ const ContentDiv = styled.div`
     top: 50%;
     left: 50%;
     transform: translate(0%, -50%);
+
+    @media (max-width: 420px) {
+        display: none;
+    }
 `;
 
-const SpeakersSection = () => {
 
-    const speakers = [
-        {
-            name: 'someone famous1',
-            image: 'https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
-            jobs: ['CEO of google', 'web dev'],
-        },
-        {
-            name: 'someone famous2',
-            image: 'https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
-            jobs: ['CEO of facebook'],
-        },
-        {
-            name: 'someone famous3',
-            image: 'https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
-            jobs: ['CEO of microsoft', 'app developer'],
-        },
-        {
-            name: 'someone famous4',
-            image: 'https://images.unsplash.com/photo-1568602471122-7832951cc4c5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80',
-            jobs: ['AI developer'],
-        },
-    ];
+const SpeakersSection = ({ speakers }) => {
 
     const [activeSlide, setActiveSlide] = useState(0);
 
     return (
-        <section className="p-10 flex flex-col justify-start items-start gap-4">
-            <h1 className="text-5xl text-blue-600 font-bold">Speakers</h1>
+        <section className="p-10 flex flex-col justify-start items-start gap-8 relative">
+            <div className="binary-data">1010111010110111000101010010010110101101110001010100110</div>
+
+            <p className="code">
+                event devfest21 = Devfest(edition=2021, host='GDGAlgiers')<br/><br/>
+
+                //Learn together<br/>
+                devfest21.setLearnTogether(true)<br/><br/>
+
+                //Celebrate it<br/>
+                devfest21.setHavefun(true)<br/><br/>
+
+                //Hack together<br/>
+                devfest21.setHackMode(true)<br/>
+            </p>
+
+            <h1 className="text-5xl text-blue font-bold">Speakers</h1>
             <p className="text-lg text-gray-600 font-normal font-mono">They are making it happens!</p>
 
-            <div className="py-12 px-8 w-full h-80 bg-blue-500 flex justify-center items-center relative">
+            <div className="py-12 px-8 w-full h-80 bg-blue flex justify-center items-center relative">
+
+                <div className="white-border left"></div>
+                <div className="white-border right"></div>
+
                 <ContentDiv>
                     <h3 className="text-4xl font-black capitalize font-mono text-black">{speakers[activeSlide].name}</h3>
                     {
-                        speakers[activeSlide].jobs.map(job => (
-                            <p className="text-lg text-gray-500 font-mono">{job}</p>
+                        speakers[activeSlide].jobs.map((job, index) => (
+                            <p key={index} className="text-lg text-gray-500 font-mono">{job}</p>
                         ))
                     }
                 </ContentDiv>
                 <Swiper
-                    slidesPerView={3}
+                    slidesPerView={window.innerWidth > 420 ? 3 : 1}
                     modules={[Navigation]}
                     navigation
                     loop={true}
                     centeredSlides={true}
                     onSlideNextTransitionEnd={(swiper) => {
-                        setActiveSlide(value => value+1 > speakers.length-1 ? 0 : value+1);
+                        setActiveSlide(value => value + 1 > speakers.length - 1 ? 0 : value + 1);
                     }}
                     onSlidePrevTransitionEnd={(swiper) => {
-                        setActiveSlide(value => value-1 < 0 ? speakers.length-1 : value-1);
+                        setActiveSlide(value => value - 1 < 0 ? speakers.length - 1 : value - 1);
                     }}
                 >
                     {
-                        speakers.map(speaker => (
-                            <SwiperSlide>
+                        speakers.map((speaker, index) => (
+                            <SwiperSlide key={index}>
                                 <Speaker {...speaker} />
                             </SwiperSlide>
                         ))
