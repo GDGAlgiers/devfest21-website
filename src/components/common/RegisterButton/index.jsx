f70e91bc9c8a5c7ccbab3f8a9a8db9755201dcac
akram09 <a.boutouchent@esi-sba.dz>
2021-10-23 19:55:35 +0100

    Finished fixing conlficts

--- src/components/common/RegisterButton/index.jsx
+++ src/components/common/RegisterButton/index.jsx
@@ -1,38 +1,33 @@
 import React from 'react'
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
-const notify= () =>toast.info("Wow so easy!")
-const RegisterButton = ({ classes}) => (
+
+const RegisterButton = ({ classes }) => (
     <div>
-    <button
-        
-        type="button"
-        className={`relative border-2 border-white-lighter bg-darkBlue text-white-lighter ${classes} `}
-    >
-        REGISTER
-        <svg
-            width="6"
-            className="absolute bottom-1 right-2"
-            height="7"
-            viewBox="0 0 6 7"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
+        <button
+            type="button"
+            className={`relative border-2 border-white-lighter bg-darkBlue text-white-lighter ${classes} `}
         >
-            <circle cx="2.95837" cy="3.12793" r="2.94006" fill="#202124" />
-        </svg>
-        <svg
-            width="6"
-            height="7"
-            className="absolute bottom-1 right-4"
-            viewBox="0 0 6 7"
-            fill="none"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <circle cx="2.95837" cy="3.12793" r="2.94006" fill="#202124" />
-        </svg>
-    </button>
-    <ToastContainer
-    position="top-center"/>
+            REGISTER
+            <svg
+                width="6"
+                className="absolute bottom-1 right-2"
+                height="7"
+                viewBox="0 0 6 7"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <circle cx="2.95837" cy="3.12793" r="2.94006" fill="#202124" />
+            </svg>
+            <svg
+                width="6"
+                height="7"
+                className="absolute bottom-1 right-4"
+                viewBox="0 0 6 7"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+            >
+                <circle cx="2.95837" cy="3.12793" r="2.94006" fill="#202124" />
+            </svg>
+        </button>
     </div>
 )
-export default RegisterButton; 
+export default RegisterButton
