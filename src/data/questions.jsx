cb7c2b3e35da1b216178ae6aec7aff67a6e16352
Zouambia Sohaib <js_zouambia@esi.dz>
2021-11-13 13:32:53 +0100

    added more questions to FAQ section

--- src/data/questions.jsx
+++ src/data/questions.jsx
@@ -14,5 +14,25 @@ const questions = [
         title: 'Is it free?',
         content: 'Absolutly, DevFest Algiers has and will always be totally FREE!',
     },
+    {
+        id: 4,
+        title: 'What\'s Live AI?',
+        content: 'Live AI is the chosen theme for this year\'s hackathon, which consists on creating AI based solutions to daily life problems.',
+    },
+    {
+        id: 5,
+        title: 'Will there be mentors?',
+        content: 'We will bring a group of qualified mentors to help you in the process and share their experiences with you and your team.',
+    },
+    {
+        id: 6,
+        title: 'How can workshops help us?',
+        content: 'Many workshops will be related to the hackathon theme and the needed techniques and technologies to make your ideas come true.',
+    },
+    {
+        id: 7,
+        title: 'What do I gain from participating?',
+        content: 'By participating in a the DevFest Algiers Hackathon you\'ll strengthen your soft skills by working in a team and sharpen your technical skills as well',
+    },
 ]
 export default questions
